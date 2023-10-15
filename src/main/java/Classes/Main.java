f07a1cfe8d522f0c762aa81a8975191ffd87cc9e
KhalilMS3 <kmsfouk@hiof.no>
2023-10-15 18:43:23 +0200

    Main has temporary been updated, when it runs, you get the login panel and can log in

---
+++ src/main/java/Classes/Main.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n" + ConsoleColors.ORANGE_BOLD_BRIGHT + "-------" +
+                "| Tourly | Connecting you to adventure | -------" );
+        Functions.loginPanel();
+
+
+
+    }
+
+}
