529b0abfe18f117f30922ed0c74e11e56ba6d734
Dilbrino <89363171+Dilbrino@users.noreply.github.com>
2023-10-16 10:08:54 +0200

    Added Admin class

---
+++ src/main/java/Classes/Admin.java
@@ -0,0 +1,18 @@
+package Classes;
+
+
+
+public class Admin {
+
+    public void LoggInnAdmin() {
+        /*More code goes here, such as Guide Panel etc.*/
+    }
+
+    public void administrerKontoer() {
+        /*More code goes here, such as Guide Panel etc.*/
+    }
+
+    public void oppdaterPlattformen() {
+        /*More code goes here, such as Guide Panel etc.*/
+    }
+}
