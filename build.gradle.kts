3d191c8c710fc746f8515d0e19bb7af6b4db6b04
Firas.MK <78766937+firas-mk@users.noreply.github.com>
2023-10-17 13:59:02 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
+    testImplementation("com.google.code.gson:gson:2.12.5")
 }
 
 tasks.test {
