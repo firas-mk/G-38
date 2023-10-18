c7ff98044911d7af73c2a9fb00fcedf49e3cda13
KhalilMS3 <kmsfouk@hiof.no>
2023-10-18 14:40:24 +0200

    - Switch-case code for userinput refactored - turistNavigationOptions() updated - searchAndDisplayTours() updated and refactored - getToursFromJSONFile() reads a JSON file that has tours based on the city choosen and shows it to the user.

--- src/main/java/Classes/Functions.java
+++ src/main/java/Classes/Functions.java
@@ -1,6 +1,13 @@
 package Classes;
 
+import java.io.File;
+import java.io.IOException;
+
 import java.util.Scanner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class Functions {
 
@@ -14,25 +21,26 @@ public class Functions {
         Scanner userLoginOption = new Scanner(System.in);
         Integer  userInput = Integer.valueOf(userLoginOption.nextLine());
 
-        switch (userInput){
-            case 1:
+        switch (userInput) {
+            case 1 -> {
                 loadingProgress();
                 System.out.println(ConsoleColors.YELLOW + "You are now logged in as" + ConsoleColors.RED_BOLD_BRIGHT + " [Turist]" + ConsoleColors.RESET);
                 turistNavigationOptions();
-                break;
-            case 2:
+            }
+            case 2 -> {
                 loadingProgress();
                 System.out.println(ConsoleColors.YELLOW + "You are now logged in as" + ConsoleColors.RED_BOLD_BRIGHT + " [Guide / Company]" + ConsoleColors.RESET);
-                /*More code goes here, such as Guide Panel etc.*/
-                break;
-            case 3:
+            }
+            /*More code goes here, such as Guide Panel etc.*/
+            case 3 -> {
                 loadingProgress();
                 System.out.println(ConsoleColors.YELLOW + "You are now logged in as" + ConsoleColors.RED_BOLD_BRIGHT + " [Admin]" + ConsoleColors.RESET);
-                /*More code goes here, such as Admin Panel etc.*/
-                break;
-            default:
+            }
+            /*More code goes here, such as Admin Panel etc.*/
+            default -> {
                 System.out.println(ConsoleColors.RED + "▢ invalid number! Enter 1, 2 or 3 " + ConsoleColors.RESET);
                 loginPanel();
+            }
         }
     }
     public static void loadingProgress(){
@@ -79,13 +87,13 @@ public class Functions {
      * </ul>
     */ //DESC-Javadoc ---> turistNavigatonOptions()
     public static void turistNavigationOptions(){
-        System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\n-------------------------------------------------------------");
+        System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\n-----------------------------------------------------------------");
         //System.out.println("| In order to navigate, choose one of the options below:    |");
-        System.out.println("|  You are now in the main menu, choose one of the options below:    |");
-        System.out.println("-------------------------------------------------------------" + ConsoleColors.RESET);
-        System.out.println(ConsoleColors.ORANGE_BOLD_BRIGHT + "[1] Search Torus\n" +
-                                                              "[2] Show favorite\n" +
-                                                              "[3] Log out" + ConsoleColors.RESET);
+        System.out.println("| You are now in the main menu, choose one of the options below |");
+        System.out.println("-----------------------------------------------------------------" + ConsoleColors.RESET);
+        System.out.println(ConsoleColors.ORANGE_BOLD_BRIGHT + "[1] Search Tours\n" +
+                                                              "[2] Show Favorite\n" +
+                                                              "[3] Log Out" + ConsoleColors.RESET);
         Scanner userLoginOption = new Scanner(System.in);
         Integer  userInput = Integer.valueOf(userLoginOption.nextLine());
         switch (userInput) {
@@ -98,67 +106,106 @@ public class Functions {
 
                 break;
             case 3:
-
-                // Implement logout logic
-
+                // (KHALIL) NEW: log out logic has been implemented
+                System.out.println(ConsoleColors.YELLOW + "|| You are successfully logged out, See you later :) ||" + ConsoleColors.RESET);
+                Scanner userInputScanner = new Scanner(System.in);
+                System.out.println(ConsoleColors.YELLOW +"||> Do you want to login? [y/n]: " + ConsoleColors.RESET);
+                String userChoice = String.valueOf(userInputScanner.nextLine());
+                if(userChoice.equals("y") || userChoice.equals("Y")){
+                    loginPanel();
+                }else {
+                    System.out.println(ConsoleColors.YELLOW + "Thank you for using Tourly. See you later ;)" + ConsoleColors.RESET);
+                }
                 break;
             default:
-                System.out.println(ConsoleColors.RED + "▢ Invalid number! Enter 1, 2, or 3 " + ConsoleColors.RESET);
+                System.out.println(ConsoleColors.RED + "◆ Invalid number! Enter 1, 2, or 3 " + ConsoleColors.RESET);
                 turistNavigationOptions();
         }
     }
 
+    // (KHALIL) NEW: implementation of searchAndDisplayTours() function, and JSON-files with data has been added
     public static void searchAndDisplayTours() {
-        System.out.println("Available Cities:");
-        System.out.println("1 - Oslo");
-        System.out.println("2 - Bergen");
-        System.out.println("3 - Kristiansand");
-        System.out.println("4 - Halden");
+        System.out.println(ConsoleColors.YELLOW_BOLD_BRIGHT + ConsoleColors.YELLOW_UNDERLINED + "Available Cities:" + ConsoleColors.RESET + ConsoleColors.ORANGE_BOLD_BRIGHT
+                + "\n[1] Oslo"
+                + "\n[2] Bergen"
+                + "\n[3] Kristiansand"
+                + "\n[4] Halden" + ConsoleColors.RESET
+        );
 
         Scanner userInputScanner = new Scanner(System.in);
         int selectedCity = -1;
 
         while (selectedCity < 1 || selectedCity > 4) {
-            System.out.print("Enter the number of the city you want to explore (1-4): ");
+            System.out.println(ConsoleColors.YELLOW +"||> Enter the number of the city you want to explore [1-4]: " + ConsoleColors.RESET);
             selectedCity = Integer.parseInt(userInputScanner.nextLine());
         }
-
-        // Now, you can add tours for the selected city
-
-        String[] cityTours = null;
-
         switch (selectedCity) {
             case 1: // Oslo
-                cityTours = new String[]{"Tour 1: Oslo City Tour", "Tour 2: Oslo Fjord Cruise"};
+                System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\n---------------------------");
+                System.out.println("| Available tours in Oslo |");
+                System.out.println("---------------------------" + ConsoleColors.RESET);
+                Functions.getToursFromJSONfile("src/main/java/JSON_files/oslo_tours.json");
                 break;
             case 2: // Bergen
-                cityTours = new String[]{"Tour 1: Bergen Harbor Tour", "Tour 2: Bergen Mountain Hike"};
+                System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\n-----------------------------");
+                System.out.println("| Available tours in Bergen |");
+                System.out.println("-----------------------------" + ConsoleColors.RESET);
+                Functions.getToursFromJSONfile("src/main/java/JSON_files/bergen_tours.json");
                 break;
             case 3: // Kristiansand
-                cityTours = new String[]{"Tour 1: Kristiansand Beach Day", "Tour 2: Kristiansand Cultural Tour"};
+                System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\n-----------------------------------");
+                System.out.println("| Available tours in Kristiansand |");
+                System.out.println("-----------------------------------" + ConsoleColors.RESET);
+                Functions.getToursFromJSONfile("src/main/java/JSON_files/kristiansand_tours.json");
                 break;
             case 4: // Halden
-                cityTours = new String[]{"Tour 1: Halden Castle Tour", "Tour 2: Halden Forest Walk"};
+                System.out.println(ConsoleColors.GREEN_BOLD_BRIGHT + "\n-----------------------------");
+                System.out.println("| Available tours in Halden |");
+                System.out.println("-----------------------------" + ConsoleColors.RESET);
+                Functions.getToursFromJSONfile("src/main/java/JSON_files/halden_tours.json");
                 break;
         }
 
-        System.out.println("Tours in the selected city:");
-        for (int i = 0; i < cityTours.length; i++) {
-            System.out.println((i + 1) + " - " + cityTours[i]);
-        }
-
-        int selectedTour = -1;
-
-        while (selectedTour < 1 || selectedTour > cityTours.length) {
-            System.out.print(" Enter the number of the tour you want to book (1-" + cityTours.length + "): ");
-            selectedTour = Integer.parseInt(userInputScanner.nextLine());
-        }
-
-        // You can add further logic here to handle the selected tour, e.g., book it or perform other actions.
     }
 
+    // (KHALIL) NEW: implementation of getToursFromJSONFile() function, now data from JSON files can be readed
+    public static void getToursFromJSONfile(String file){
+       try{
+           // ObjectMapper to read from the JSON file
+           ObjectMapper objectMapper = new ObjectMapper();
+
+           //read JSON-filen
+           JsonNode jsonFile = objectMapper.readTree(new File(file));
+           // if the json-file is empty (which means if there is no tours available) a message will be printed to the user
+           if (jsonFile.isEmpty()){
+               System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "|| Oops, looks like there is no tours available! ||" + ConsoleColors.RESET);
+               searchAndDisplayTours();
+           }else {
+           Integer tourNr = 0;
+           //Iterate throw and get information
+           for(JsonNode tour : jsonFile){
+               tourNr++;
+               String location = tour.get("location").asText();
+               String date = tour.get("date").asText();
+               String time = tour.get("time").asText();
+               String description = tour.get("description").asText();
+               String price = tour.get("price").asText();
+
+               System.out.println(
+                       "\n"+ ConsoleColors.YELLOW_UNDERLINED + ConsoleColors.YELLOW_BOLD_BRIGHT+ "[*] Tour number " + ConsoleColors.RED_BOLD_BRIGHT +  tourNr + ConsoleColors.RESET +
+                       "\n" + ConsoleColors.RED_BOLD_BRIGHT +"[*] " + ConsoleColors.RESET + "Location: " + location +
+                       "\n" + ConsoleColors.RED_BOLD_BRIGHT +"[*] " + ConsoleColors.RESET + "Date: " + date +
+                       "\n" + ConsoleColors.RED_BOLD_BRIGHT +"[*] " + ConsoleColors.RESET + "Time: " + time +
+                       "\n" + ConsoleColors.RED_BOLD_BRIGHT +"[*] " + ConsoleColors.RESET + "Description: " + description +
+                       "\n" + ConsoleColors.RED_BOLD_BRIGHT +"[*] " + ConsoleColors.RESET + "Price: " + price
+               );
+           }
+       }} catch (IOException e) {
+           e.printStackTrace();
+       }
 
 
+    }
 
 
 }
