84cecb2321c3c8f722ac697288a43c1c9888393b
KhalilMS3 <kmsfouk@hiof.no>
2023-10-18 14:22:51 +0200

    Gradle Update - json dependencies has been added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.7.1")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.7.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
