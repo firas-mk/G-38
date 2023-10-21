da3f72f64d5b505991f88f461e038588d56d19bb
Firas.MK <78766937+firas-mk@users.noreply.github.com>
2023-10-21 16:25:57 +0200

    Define the Turist class with basic attributes and methods Define the Admin class with basic attributes and methods
    Implement JSON serialization (saveToJson and loadFromJson) in all classs

--- src/main/java/Classes/Admin.java
+++ src/main/java/Classes/Admin.java
@@ -1,14 +1,56 @@
 package Classes;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileReader;
 
 
 public class Admin {
+    private String adminId;
+    private String name;
+    private String contactInformation;
 
-    public void administrerKontoer() {
-        /*More code goes here*/
+    public Admin(String adminId, String name, String contactInformation) {
+        this.adminId = adminId;
+        this.name = name;
+        this.contactInformation = contactInformation;
     }
 
-    public void oppdaterPlattformen() {
-        /*More code goes here.*/
+    public String getAdminId() {
+        return adminId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void saveToJson(String filename) {  /* ?????????????*/
+        try (FileWriter writer = new FileWriter(filename)) {
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<Admin>>() {}.getType();
+            gson.toJson(this, listType, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+  public static Admin loadFromJson(String filename) {
+        try {
+            Gson gson = new Gson();
+            Admin admin = gson.fromJson(new FileReader(filename), Admin.class);
+            return admin;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- src/main/java/Classes/Turist.java
+++ src/main/java/Classes/Turist.java
@@ -1,17 +1,40 @@
 package Classes;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileReader;
 
 import java.util.ArrayList;
 
 public class Turist  {
-    private ArrayList<String> tourList;
+    private String touristId;
+    private String name;
+    private String contactInformation;
+    private List<Booking> bookings;
     private ArrayList<String> favoriteList;
 
-    public ArrayList<String> getTourList() {
-        return tourList;
+    public String getTouristId() {
+        return touristId;
     }
 
-    public void setTourList(ArrayList<String> tourList) {
-        this.tourList = tourList;
+    public String getName() {
+        return name;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;   /* _????????????????????*/
+    }
+
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
     }
 
     public ArrayList<String> getFavoriteList() {
@@ -24,5 +47,25 @@ public class Turist  {
 
     public static String searchTour(){
         return "Hei";
+    }
+    public void saveToJson(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<Turist>>() {}.getType();
+            gson.toJson(this, listType, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Turist loadFromJson(String filename) {
+        try {
+            Gson gson = new Gson();
+            Turist turist = gson.fromJson(new FileReader(filename), Turist.class);
+            return turist;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
