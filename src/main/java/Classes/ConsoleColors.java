0ee1cf349dcb3fa9614f0f8effb84c0b4b03abf9
KhalilMS3 <kmsfouk@hiof.no>
2023-11-05 22:39:22 +0100

    AnsiCode for orange color changed

--- src/main/java/Classes/ConsoleColors.java
+++ src/main/java/Classes/ConsoleColors.java
@@ -1,5 +1,10 @@
 package Classes;
 
+/**
+*
+* <b>Code Imported from:</b>
+* https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+* */
 public class ConsoleColors {
     // Reset
     public static final String RESET = "\033[0m";  // Text Reset
@@ -59,7 +64,7 @@ public class ConsoleColors {
     public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
     public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
     public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
-    public static final String ORANGE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
+    public static final String ORANGE_BOLD_BRIGHT = "\u001B[1;38;5;208m";  // ORANGE
     public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
     public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
     public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
