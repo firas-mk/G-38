420384d1ebdd3d6e7a44d142824f63021571e28a
firasmk <firas.mkdm@gmail.com>
2023-11-14 18:16:32 +0100

    Fix gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,28 @@ dependencies {
 
     implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.7.1")
     implementation("junit:junit:4.13.1")
-    implementation("junit:junit:4.13.1")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.8.1")
 
 }
 
-tasks.test {
+tasks.named<Test>("test") {
     useJUnitPlatform()
+    include ("*Test.java")
+    maxHeapSize = "1G"
+
+    testLogging {
+        events("passed")
+    }
+}
+sourceSets {
+    test {
+        java {
+            srcDir ("src/test/java/Classes")
+        }
+    }
 }
