8c0283a1e0a13f2d0efb8fbd73210f9c99f1e4cb
firasmk <firas.mkdm@gmail.com>
2023-11-18 08:50:40 +0100

    Test Admin Functions: Add, Edit, and Delete Tours - Added test case for adding a tour: `testAddTour()` - Added test case for editing a tour: `testEditTour()` - Added test case for deleting a tour: `testDeleteTour()`

--- src/test/java/UserPanelTest.java
+++ src/test/java/UserPanelTest.java
@@ -98,4 +98,102 @@ public class UserPanelTest {
         assertFalse(actualOutput.contains(expectedTourStatus));
     }
 
+    @Test
+    public void testAddTour() {
+
+        String testFilePath = "src/main/java/JSON_files/test_tours.json";
+        int cityNumber = 1;
+
+        String simulatedUserInput = cityNumber + "\nLocation1\n2023-01-01\n10:00\nA beautiful tour\n500 kr\n";
+        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+        Admin.addTour();
+        String expectedOutput = "Tour added successfully!";
+        String actualOutput = outputStream.toString();
+        assertTrue(actualOutput.contains(expectedOutput), "Tour was not added successfully");
+
+        System.setIn(System.in);
+    }
+
+    @Test
+    public void testEditTour() {
+        String testFilePath = "src/main/java/JSON_files/test_tours.json";
+        int cityNumber = 1;
+        int tourNumberToEdit = 2;
+
+        String simulatedUserInput = cityNumber + "\n" + tourNumberToEdit + "\nNew Location\n2023-01-02\n11:00\nUpdated description\n550 kr\n";
+        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+        Admin.editTour();
+        String expectedOutput = "Tour edited successfully!";
+        String actualOutput = outputStream.toString();
+        assertTrue(actualOutput.contains(expectedOutput), "Tour was not edited successfully");
+
+
+        System.setIn(System.in);
+    }
+
+    @Test
+    public void testDeleteTour() {
+
+        String testFilePath = "src/main/java/JSON_files/test_tours.json";
+        int cityNumber = 1;
+        int tourNumberToDelete = 3;
+
+        Admin.deleteTour(cityNumber, tourNumberToDelete);
+        String expectedOutput = "Tour deleted successfully!";
+        String actualOutput = outputStream.toString();
+        assertTrue(actualOutput.contains(expectedOutput), "Tour was not deleted successfully");
+    }
+
+    /*@Test
+    public void testBookTourSuccess() {
+        // Arrange
+        // Assuming that "1" is the city number and "3" is the tour number for the test
+        String input1 = "1";
+        String input2 = "3";
+        /*System.setIn(new ByteArrayInputStream(input1.getBytes()));
+        System.setIn(new ByteArrayInputStream(input2.getBytes()));*/
+/*
+        // Assuming you have a method to set the test file path for the booking
+        String testFilePath = "src/main/java/JSON_files/oslo_tours.json";
+        //Admin.setFilePathForTesting(testFilePath); // Set the file path to a test file
+        String simulatedUserInput = input1 + "\n" + input2 + "\nNew Location\n2023-01-02\n11:00\nUpdated description\n550 kr\n";
+        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+
+        // Act
+        Guide.bookTour("guideid");
+
+        // Assert
+        // Verify the output on the console
+        String expectedOutput = "Tour booked successfully!";
+        String actualOutput = outputStream.toString();
+        assertTrue(actualOutput.contains(expectedOutput));
+
+        // Optionally, read the test file and check if the tour status is updated to 'available'
+        // and the guideID is set to "guide123"
+    }*/
+   /* @Test
+    public void testBookTour() {
+        // Simulate user inputs
+        String input = "2\n5\n"; // Example: city number 2 and tour number 5
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        // Mocking file and other dependencies
+        // ... (Depends on your implementation)
+
+        // Call the method
+        Guide.bookTour("guide123");
+
+        // Verify the output
+        assertTrue(outputStreamCaptor.toString().contains("Tour booked successfully"));
+
+        // Verify changes in the JSON file
+        // ... (Read the JSON file and check for updates)
+
+        // Reset System.in to its original state
+        System.setIn(System.in);
+    }*/
+
 }
